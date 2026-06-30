ef0f03f00a3d0c466377aa7063c6c7b81a0be38f
Joseph K. Arman, Jr. <161454734+josephkarmanjr@users.noreply.github.com>
2026-06-30 20:10:02 +0200

    Add files via upload

---
+++ sql_scripts/task1_pdb_creation.sql
@@ -0,0 +1,6 @@
+CREATE PLUGGABLE DATABASE jo_pdb_19939_2022
+ADMIN USER pdbadmin IDENTIFIED BY Joseph123
+FILE_NAME_CONVERT = (
+'C:\app\josep\product\21c\oradata\XE\pdbseed\',
+'C:\app\josep\product\21c\oradata\XE\JO_PDB_19939_2022\'
+);
---
+++ sql_scripts/task2_temp_pdb.sql
@@ -0,0 +1,6 @@
+CREATE PLUGGABLE DATABASE jo_to_delete_pdb_19939_2022
+ADMIN USER tempadmin IDENTIFIED BY Temp123
+FILE_NAME_CONVERT = (
+'C:\app\josep\product\21c\oradata\XE\pdbseed\',
+'C:\app\josep\product\21c\oradata\XE\JO_TO_DELETE_PDB_19939_2022\'
+);
